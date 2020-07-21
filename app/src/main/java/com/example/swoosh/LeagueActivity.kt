e422d3352f1276ff2c16f11fa2834c7f88628970
Jun Park <68208417+JunYou-Park@users.noreply.github.com>
2020-07-21 10:18:20 +0900

    Update LeagueActivity
    LeagueActivity의 toggle 버튼의 기능을 추가
    LeagueActivity에서 SkillActivity로 전환될 때 selectedLeague값을 전달

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,21 +1,50 @@
 package com.example.swoosh
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
-    fun leagueNextClicked(view: View){
+    fun leagueClicked(view: View){
         when(view.id){
             R.id.btn_league_next->{
-                val skillActivity = Intent(this, SkillActivity::class.java)
-                startActivity(skillActivity)
+                if(toggle_league_mens.isChecked||toggle_league_womens.isChecked||toggle_league_coed.isChecked) {
+                    val skillActivity = Intent(this, SkillActivity::class.java)
+                    skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+                    startActivity(skillActivity)
+                }
+                else{
+                    Toast.makeText(this, "Please select a league." , Toast.LENGTH_SHORT).show()
+                }
+            }
+            R.id.toggle_league_mens->{
+                toggle_league_womens.isChecked = false
+                toggle_league_coed.isChecked = false
+
+                selectedLeague = "mens"
+            }
+            R.id.toggle_league_womens->{
+                toggle_league_coed.isChecked = false
+                toggle_league_mens.isChecked = false
+
+                selectedLeague = "womens"
+
+            }
+            R.id.toggle_league_coed->{
+                toggle_league_mens.isChecked = false
+                toggle_league_womens.isChecked = false
+
+                selectedLeague = "co-ed"
             }
         }
     }
