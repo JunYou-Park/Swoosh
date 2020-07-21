7128aa79725f4bbe460d5a387e9d03ff45e84ae4
Jun Park <68208417+JunYou-Park@users.noreply.github.com>
2020-07-21 09:29:11 +0900

    Add SkillActivity
    1. layout_skill을 구현
    2. leagueActivity에서 SkillActivity로 전환 구현

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,11 +1,22 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View){
+        when(view.id){
+            R.id.btn_league_next->{
+                val skillActivity = Intent(this, SkillActivity::class.java)
+                startActivity(skillActivity)
+            }
+        }
     }
 }
